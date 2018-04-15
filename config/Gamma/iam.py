6f010e2c5514e7bd7b9d9babcdecd2f5c4d741a2
Janos Potecki <janospotecki@Janoss-MacBook.local>
2018-04-15 09:39:45 +0200

    Init commit

---
+++ config/Gamma/iam.py
@@ -0,0 +1,20 @@
+from awslambdacontinuousdelivery.tools.iam import (
+    defaultAssumeRolePolicyDocument
+  , oneClickCreateLogsPolicy
+  )
+
+from troposphere import Sub
+from troposphere.iam import Role, Policy
+import awacs.aws
+
+def get_iam(ref_name: str) -> Role:
+  assume = defaultAssumeRolePolicyDocument("lambda.amazonaws.com")
+  return Role( ref_name
+             , RoleName = ref_name
+             , AssumeRolePolicyDocument = assume
+             , Policies = [ oneClickCreateLogsPolicy() ]
+             )
+
+if __name__ == "__main__":
+  role = get_iam("Test")
+  print(str(role.to_dict()))
---
+++ src/function.py
@@ -0,0 +1,15 @@
+import os
+from time import time
+import boto3
+
+def lambda_handler(event, context):
+    timestamp = time() # time of arrival
+    table = s.environ["tablename"]
+    client = boto3.client("dynamodb")
+
+    item = {}
+    item["sensorId"] = {"N" : str(event["id"]) }
+    item["timestamp"] = { "N" : str(timestamp) }
+    item["moisture"] = {"N" : str(event["moisture"]) }
+    resp = client.put_item(TableName = table, Item = item)
+    return resp
